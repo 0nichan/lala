baa845b62ac6c31a83b1c8e0daa41cf77f81dfa0
0nichan <43495942+0nichan@users.noreply.github.com>
2018-09-26 08:16:12 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<? php
+// masukan config token yg tersedia pada link di packet captur
+// jangan hapus tanda kutip ("")
+$ access_token  =  " d445afb1-2104-419d-a614-18bb7d982342 " ;
+Anda dapat membuat banyak konfigurasi untuk beberapa akun.
